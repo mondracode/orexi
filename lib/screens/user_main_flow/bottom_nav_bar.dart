3dd9d0abdd5b17463f2147e24e02ed6a0f2de65b
Santiago Mondragón Gómez <smondragon@unal.edu.co>
2020-10-31 19:05:00 -0500

    login test

--- lib/screens/user_main_flow/bottom_nav_bar.dart
+++ lib/screens/user_main_flow/bottom_nav_bar.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:orexi/constants.dart';
 
@@ -29,6 +30,15 @@ class _MyBottomNavigationBarState extends State<MyBottomNavigationBar> {
 
   @override
   Widget build(BuildContext context) {
+    FirebaseAuth.instance.authStateChanges().listen((User user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print('User is signed in!');
+        print(user.email);
+      }
+    });
+
     return Scaffold(
       body: _children[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
