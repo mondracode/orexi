fb0072a2867fc270f7e6fb816a644a1d3b9bb4c8
Santiago Mondragón Gómez <smondragon@unal.edu.co>
2020-11-02 23:35:01 -0500

    early database fetching implemented

---
+++ lib/classes/producto.dart
@@ -0,0 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Producto {
+  String nombre, descripcion;
+  double distancia, precio;
+
+  Producto(this.nombre, this.descripcion, this.distancia, this.precio);
+
+  Producto.fromSnapshot(DocumentSnapshot snapshot) {
+    this.nombre = snapshot["nombre"];
+    this.descripcion = snapshot["descripcion"];
+    this.precio = snapshot["precio"];
+    this.distancia = snapshot["distancia"];
+  }
+}
--- lib/screens/user_main_flow/cerca.dart
+++ lib/screens/user_main_flow/cerca.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:orexi/classes/producto.dart';
 import 'package:orexi/constants.dart';
 import 'package:orexi/screens/user_main_flow/components/background.dart';
 import 'package:orexi/screens/user_main_flow/components/near_product.dart';
@@ -37,45 +38,83 @@ class _CercaState extends State<Cerca> {
         ],
       ),
       body: Background(
-        child: ListView(
-          padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
-          children: <Widget>[
-            SearchField(
-              hintText: "¿Qué quieres comer?",
-            ),
-            SizedBox(height: size.height * 0.03),
-            NearProduct(
-              productImage: 'assets/images/placeholder.png',
-              productName: "Dos porciones de arroz",
-              productDesc: "Dos porciones de 60gr de arroz blanco y fideos",
-              productPrice: 8000,
-              productDistance: 20,
-              press: () {},
-            ),
-            NearProduct(
-              productImage: 'assets/images/placeholder.png',
-              productName: "Tres alas de pollo",
-              productDesc: "Tres allas de pollo a la broaster",
-              productPrice: 9500,
-              productDistance: 48,
-              press: () {},
-            ),
-            NearProduct(
-              productImage: 'assets/images/placeholder.png',
-              productName: "Seis panes hojaldrados",
-              productDesc: "Seis panes holaldrados horneados hoy en la mañana",
-              productPrice: 2500,
-              productDistance: 69,
-              press: () {},
-            ),
-          ],
+        child: Container(
+          padding: EdgeInsets.all(16),
+          child: StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance
+                .collection('establecimiento')
+                .limit(20)
+                .snapshots(),
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (!snapshot.hasData) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+
+              return ListView(
+                children: snapshot.data.docs.map((DocumentSnapshot doc) {
+                  return NearProduct(
+                    productImage: 'assets/images/placeholder.png',
+                    productName: doc["nombre"],
+                    productDesc: doc["direccion"],
+                    productPrice: 420,
+                    productDistance: 69,
+                    press: () {},
+                  );
+                }).toList(),
+              );
+            },
+          ),
         ),
+        // child: ListView(
+        //   padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
+        //   children: <Widget>[
+        //     SearchField(
+        //       hintText: "¿Qué quieres comer?",
+        //     ),
+        //     SizedBox(height: size.height * 0.03),
+
+        //     // NearProduct(
+        //     //   productImage: 'assets/images/placeholder.png',
+        //     //   productName: "Dos porciones de arroz",
+        //     //   productDesc: "Dos porciones de 60gr de arroz blanco y fideos",
+        //     //   productPrice: 8000,
+        //     //   productDistance: 20,
+        //     //   press: () {},
+        //     // ),
+        //     // NearProduct(
+        //     //   productImage: 'assets/images/placeholder.png',
+        //     //   productName: "Tres alas de pollo",
+        //     //   productDesc: "Tres allas de pollo a la broaster",
+        //     //   productPrice: 9500,
+        //     //   productDistance: 48,
+        //     //   press: () {},
+        //     // ),
+        //     // NearProduct(
+        //     //   productImage: 'assets/images/placeholder.png',
+        //     //   productName: "Seis panes hojaldrados",
+        //     //   productDesc: "Seis panes holaldrados horneados hoy en la mañana",
+        //     //   productPrice: 2500,
+        //     //   productDistance: 69,
+        //     //   press: () {},
+        //     // ),
+        //   ],
+        // ),
       ),
     );
   }
 
-  // Stream<QuerySnapshot> loadNearProducts(){
-  //   return FirebaseFirestore.instance.collection('collectionPath')
-  // }
+  Stream<QuerySnapshot> loadNearProducts() {
+    return FirebaseFirestore.instance
+        .collection('establecimiento/producto')
+        .orderBy('distancia', descending: false)
+        .limit(20)
+        .snapshots();
+  }
 
+  List<Producto> getProductsFromQuery(QuerySnapshot snapshot) {
+    return snapshot.docs.map((DocumentSnapshot doc) {}).toList();
+  }
 }
