b5b509bdce89465d5dc6f3298b754fcbc9c1d5a2
Santiago Mondragón Gómez <smondragon@unal.edu.co>
2020-12-14 10:59:45 -0500

    add historial de reservas cliente

--- lib/screens/user_main_flow/historial_reservas.dart
+++ lib/screens/user_main_flow/historial_reservas.dart
@@ -1,7 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:orexi/constants.dart';
 import 'package:orexi/screens/user_main_flow/components/background.dart';
 import 'package:orexi/screens/user_main_flow/components/bought_product.dart';
+import 'package:orexi/screens/user_main_flow/components/near_product.dart';
 import 'package:orexi/screens/user_main_flow/components/search_field.dart';
 
 class Historial extends StatefulWidget {
@@ -10,6 +13,7 @@ class Historial extends StatefulWidget {
 }
 
 class _HistorialState extends State<Historial> {
+  static User user = FirebaseAuth.instance.currentUser;
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -39,35 +43,37 @@ class _HistorialState extends State<Historial> {
         ], */
       ),
       body: Background(
-        child: ListView(
-          padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
-          children: <Widget>[
-            SearchField(
-              hintText: "¿Alguna reserva en especial?",
-            ),
-            SizedBox(height: size.height * 0.03),
-            BoughtProduct(
-              productImage: 'assets/images/placeholder.png',
-              productName: "Tres porciones de frijoles",
-              restaurantName: "Los 69 patos.com",
-              productPrice: 69420,
-              press: () {},
-            ),
-            BoughtProduct(
-              productImage: 'assets/images/placeholder.png',
-              productName: "Pee pee poo poo",
-              restaurantName: "Esternocleidomastoideo Inc",
-              productPrice: 00110110,
-              press: () {},
-            ),
-            BoughtProduct(
-              productImage: 'assets/images/placeholder.png',
-              productName: "Tengo sueño",
-              restaurantName: "Restaurante X",
-              productPrice: 123456,
-              press: () {},
-            ),
-          ],
+        child: Container(
+          padding: EdgeInsets.all(16),
+          child: StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance
+                .collection('reserva')
+                .where('cliente', isEqualTo: user.email)
+                .snapshots(),
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (!snapshot.hasData) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              return ListView(
+                children: snapshot.data.docs.map((DocumentSnapshot doc) {
+                  // return Text(doc['nombre']);
+                  return NearProduct(
+                    productId: doc.id,
+                    productImage: 'assets/images/placeholder.png',
+                    productName: doc["nombre"],
+                    productPrice: doc["precio"],
+                    productQuantity: doc["unidades"],
+                    productDesc: doc['fecha'],
+                    productDistance: 0,
+                    press: () {},
+                  );
+                }).toList(),
+              );
+            },
+          ),
         ),
       ),
     );
